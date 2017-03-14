5d619e3c1128c5676aad827cd63df8d5969b9eab
Michael Roque <mikeroque1@gmail.com>
2017-03-14 15:44:22 -0600

    file edit

--- migrations/20170314140715_poll_result.js
+++ migrations/20170314140715_poll_result.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('poll_result', function(table) {
     table.increments();
+    table.text('name').notNullable();
     table.integer('votes').notNullable();
     table.string('place_id').notNullable();
     table.integer('poll_id').notNullable().references('poll.id').onDelete('CASCADE');
