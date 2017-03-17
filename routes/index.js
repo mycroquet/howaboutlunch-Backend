b3746ddfd846344a8acd064eaed3b79951800dac
Michael Roque <mikeroque1@gmail.com>
2017-03-17 15:38:21 -0600

    more yelp code3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var Yelp = require('yelp');
+var yelp = require('yelp');
 
 var places = require('../api/googlemaps');
 var knex = require('../db/db_connection');
