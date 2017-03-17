5873fa42d6e6f9c7f854fc1a3ad2c03f7b1a7732
Michael Roque <mikeroque1@gmail.com>
2017-03-17 15:34:09 -0600

    more yelp code

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 
 var places = require('../api/googlemaps');
 var knex = require('../db/db_connection');
+var yelp = require('yelp');
 
 
 /* GET home page. */
@@ -20,14 +21,30 @@ router.get('/places', function(req, res, next) {
         y: req.query.longitude
     }
 
-    places.getPlace({
-            location: `${coords.x},${coords.y}`,
-            categories: 'restaurant'
+    yelp.search({
+            term: 'food',
+            location: `${coords.x},${coords.y}`
+
         })
-        .then(body => res.json(body))
-        .catch(err => {
-          err
+        .then(function(data) {
+            // console.log(data); // print the data returned from the API call
+            var jsonString = JSON.stringify(data); // convert data to JSON string
+            jsonBussObj = JSON.parse(jsonString).businesses; // Parse JSON string to JSON Object
+            console.log(jsonBussObj); // Print each business JSON object
+            var l = jsonBussObj.length; // Print length
         })
+        .catch(function(err) {
+            console.error(err);
+        });
+
+    // places.getPlace({
+    //         location: `${coords.x},${coords.y}`,
+    //         categories: 'restaurant'
+    //     })
+    //     .then(body => res.json(body))
+    //     .catch(err => {
+    //       err
+    //     })
 })
 
 
