9fb8d190e294378d1b568f12d760bc09120f3a1d
Michael Roque <mikeroque1@gmail.com>
2017-03-23 09:17:45 -0600

    routing stuff

--- routes/index.js
+++ routes/index.js
@@ -91,7 +91,7 @@ router.post('/poll', function(req, res, next) {
 
 /* GET Poll */
 router.get('/poll/:id', function(req, res, next) {
-    userModel.getPoll(9)
+    userModel.getPoll(43)
         .then(poll => {
             res.json({
               poll
